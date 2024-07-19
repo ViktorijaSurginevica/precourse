495a8a4c5f458447b44217ec570bd2630198feb7
”ViktorijaSurginevica” <viafuture89@gmail.com>
2024-07-19 15:50:41 +0300

    lesson 1.7-1

---
+++ lesson-01/07.js
@@ -0,0 +1,7 @@
+let iAm = "я";
+let firstChar = iAm.charAt(0);
+let upperFirstChar = firstChar.toUpperCase();
+let restOfString = iAm.slice(1);
+let result = upperFirstChar + restOfString;
+let krasavchik = "красавчик";
+console.log(result + " " + krasavchik);
