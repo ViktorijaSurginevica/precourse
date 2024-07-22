3e69e54686200dbd0115560817dd25d68849cebd
”ViktorijaSurginevica” <viafuture89@gmail.com>
2024-07-22 12:15:53 +0300

    task 1.2

--- lesson-01/08self-education.js
+++ lesson-01/08self-education.js
@@ -1,12 +1,7 @@
-//alert("Hello It-Kamasutra");
-//alert("Hello It-incubator");
-Задание 1
-//Объявите две переменные: admin и name.
-//Запишите строку "Джон" в переменную name.
-//Скопируйте значение из переменной name в admin.
-//Выведите на экран значение admin, используя функцию alert (должна показать «Джон»).
-let admin;
-let name;
-name = 'Jhon';
-name = admin;
-alert(admin);
+let admin, name; // можно объявить две переменные через запятую
+
+names = "Джон";
+
+admin = names;
+
+alert(admin); // "Джон"
