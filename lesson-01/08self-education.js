efba2335658dc09c0514dc9a47b9f778ae37d31a
”ViktorijaSurginevica” <viafuture89@gmail.com>
2024-07-22 12:12:27 +0300

    task 1.1

--- lesson-01/08self-education.js
+++ lesson-01/08self-education.js
@@ -1,2 +1,12 @@
-alert("Hello It-Kamasutra");
-alert("Hello It-incubator");
+//alert("Hello It-Kamasutra");
+//alert("Hello It-incubator");
+Задание 1
+//Объявите две переменные: admin и name.
+//Запишите строку "Джон" в переменную name.
+//Скопируйте значение из переменной name в admin.
+//Выведите на экран значение admin, используя функцию alert (должна показать «Джон»).
+let admin;
+let name;
+name = 'Jhon';
+name = admin;
+alert(admin);
