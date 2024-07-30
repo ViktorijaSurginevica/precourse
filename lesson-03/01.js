279fb51344bff6a5cf222611a2a2867083b45c4c
”ViktorijaSurginevica” <viafuture89@gmail.com>
2024-07-30 22:15:13 +0300

    lesson 3.1.1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
 // code
+​const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(color => color === "черный" || "красный" || color === "желтый").join('-');
+}
+
+console.log(createColorString());
