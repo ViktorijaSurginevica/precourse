fa402b981e6474e678d7be10e65b51750b78cc04
”ViktorijaSurginevica” <viafuture89@gmail.com>
2024-07-30 22:22:17 +0300

    lesson 3.1.3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -2,7 +2,7 @@
 ​const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString()  {
-    let germaFlag = colors.filter ( function(color) { return color === "черный"  color === "красный"  color === "желтый";    });
+    let germaFlag = colors.filter ( function(color) { return color === "черный" || color === "красный" || color === "желтый";});
     return germaFlag.join('-');
 }
 
