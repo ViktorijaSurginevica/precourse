efa97a328f81b326e66870e35086e2d1b0d9d095
”ViktorijaSurginevica” <viafuture89@gmail.com>
2024-08-01 15:50:00 +0300

    lesson 2.2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -1,18 +1,9 @@
-let passportMarried = {
-  name: "Petr",
-  surname: "Petrov",
-  address: {
-    country: "USA",
-    city: "Bobryisk",
-  },
-};
-
-let passportMarried = {
-  ...passporNottMarried,
-  address: {
-    ...passporNottMarried.address,
-  },
-};
-passportMarried.married = true;
-console.log(passporNottMarried);
-console.log(passportMarried);
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+debugger;
+console.log(factorial(5));
