1f14771fd6c41f5c23bc6348e056940d877437f3
”ViktorijaSurginevica” <viafuture89@gmail.com>
2024-08-01 22:38:43 +0300

    lesson 2.7.2

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,10 +1,18 @@
+let passporNottMarried = {
+  name: "Petr",
+  surname: "Petrov",
+  address: {
+    country: "USA",
+    city: "Bobryisk",
+  },
+};
+
 let passportMarried = {
   ...passporNottMarried,
   address: {
     ...passporNottMarried.address,
   },
 };
-passportMarried2.married = true;
-
+passportMarried.married = true;
+console.log(passporNottMarried);
 console.log(passportMarried);
-console.log(passportMarried2);
